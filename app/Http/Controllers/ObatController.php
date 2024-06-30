4215fe51377f6f9535344350c9f5b22dd74bab25
AhmadKadafi29 <kadafiahmad342@gmail.com>
2024-06-30 15:30:02 +0700

    penambahan edit obat dan detail pembelian

--- app/Http/Controllers/ObatController.php
+++ app/Http/Controllers/ObatController.php
@@ -32,7 +32,6 @@ class ObatController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'kode_obat'=>'required',
             'kategori_obat_id' => 'integer',
             'merek_obat' => 'required',
             'dosis' => 'required',
@@ -55,16 +54,16 @@ class ObatController extends Controller
     }
 
 
-    public function edit($id)
+    public function edit($id_obat)
 
     {
         $kategori=Kategoriobat::all();
-        $obat = Obat::findOrFail($id);
+        $obat = Obat::findOrFail($id_obat);
         $kategori = Kategoriobat::all();
         return view('pages.obat.edit', compact('obat', 'kategori'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id_obat)
     {
         $request->validate([
             'kategori_obat_id' => 'integer',
@@ -75,19 +74,10 @@ class ObatController extends Controller
             'efek_samping' => 'required',
 
         ]);
-        $obat = Obat::findOrFail($id);
+        $obat = Obat::findOrFail($id_obat);
         $obat->update($request->all());
 
         return redirect()->route('Obat.index')->with('success', 'Obat berhasil diubah');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        $obat = Obat::find($id);
-        $obat->delete();
-        return redirect()->route('Obat.index')->with('success', 'Obat berhasil dihapus');
-    }
 }
